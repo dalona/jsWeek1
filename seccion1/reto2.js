3c74aeba2a384ab391bdde33d9687589c64b5383
Daniel Alejandro Lopez Naranjo <dalona126@hotmail.com>
2024-04-17 15:33:59 -0500

    Change variables, fix conditionals, obj able to save

--- seccion1/reto2.js
+++ seccion1/reto2.js
@@ -2,38 +2,40 @@ const listOfSouvenirs = [];
 
 let nombre, price, availability;
 
-let areCorrectDataTypes = false;
+let areCorrectDataTypes = true;
 
-while (!areCorrectDataTypes){
+while (areCorrectDataTypes){
 
     s=prompt("Do you want to add a souvenir?").toUpperCase()
-    if (s===('YES'))
+    if  (s===('YES')){
+        nombre=prompt("Enter the name of the souvenir");
+        price= Number(prompt("Enter the price of the souvenir"));
+        availability= confirm ("Click OK if the souvenir is available");
 
-    nombre=prompt("Enter the name of the souvenir");
-    price= Number(prompt("Enter the price of the souvenir"));
-    availability= confirm ("Click OK if the souvenir is available");
+        const souvenir={
+            nombre,
+            price,
+            availability,   
+        }
+
+        listOfSouvenirs.push(souvenir)
+
+        
+
+    }else break
+    
+
 
     areCorrectDataTypes = typeof nombre=== 'string' && !isNaN(price) && availability;
     if (areCorrectDataTypes) {
         alert("You have entered correct data types")
-        break
-    } 
-}
-
-const souvenir={
-    nombre,
-    price,
-    availability,   
-}
-
-listOfSouvenirs.push(souvenir)
-
-console.log(listOfSouvenirs)
-// souvenir = {
-//     nombre: ,
-//     price:,
-//     availability:,
     
-// }
+    } else  console.log("Data entered is wrong") 
+}
+
+
+    console.log(listOfSouvenirs)
+
+
 
 
